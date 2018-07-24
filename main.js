f226f18463bfff829ad0df7aa62c51067149accf
Adam Martyn <amartyn.wrodm@yahoo.pl>
2018-07-24 18:07:28 +0200

    Changes in index.html and main.js

--- main.js
+++ main.js
@@ -23,47 +23,62 @@ scissors.addEventListener('click', function() {
     playerMove('scissors');
 });
 
-var final = function() {
-    if (userScore > computerScore) {
-        document.getElementById('board').classList.add('score-board-green');
-    } else if (userScore < computerScore) {
-        document.getElementById('board').classList.add('score-board-red');
+/*var final = function() {
+    /*    if (userScore > computerScore) {
+            document.getElementById('board').classList.add('score-board-green');
+        } else if (userScore < computerScore) {
+            document.getElementById('board').classList.add('score-board-red');
+        }
+    rock.disabled = true;
+    paper.disabled = true;
+    scissors.disabled = true;
+};*/
+var finitoGame = function() {
+    if (leftRound === 0) {
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
     }
-};
-
+}
 
 var playerMove = function(playerChoice) {
     var com = computerChoice();
     parallel(playerChoice, com);
 
-    if (leftRound === 0) {
-        final();
-    }
+    finitoGame(leftRound);
+
+    /*    var roundFi = leftRound;
+        if (roundFi === 0) {
+            rock.disabled = true;
+            paper.disabled = true;
+            scissors.disabled = true;
+        }*/
 };
 
 var computerChoice = function() {
-    var choice;
-    var randomNum = Math.floor(Math.random() * 3 + 1);
+    var randomNum = Math.floor((Math.random() * 3) + 1);
     console.log(randomNum);
     if (randomNum === 1) {
-        choice = 'paper';
+        randomNum = 'paper';
     } else if (randomNum === 2) {
-        choice = 'stone';
+        randomNum = 'rock';
     } else if (randomNum === 3) {
-        choice = 'scisssors';
+        randomNum = 'scissors';
     }
-    return choice;
+    return randomNum;
 };
 
 
 var parallel = function(playerChoice, computerChoice) {
+    console.log(playerChoice);
+    console.log(computerChoice);
     if (playerChoice === computerChoice) {
         resultMsg.innerHTML = 'it is  draw'
-    } else if ((playerChoice === 'paper' && computerChoice === 'rock') || (playerChoice === 'rock' && computerChoice === 'scissors') || (playerChoice === 'scissors' && computerChoice === 'paper')) {
+    } else if ((playerChoice === 'paper') && (computerChoice === 'rock') || ((playerChoice === 'rock') && (computerChoice === 'scissors')) || ((playerChoice === 'scissors') && (computerChoice === 'paper'))) {
         resultMsg.innerHTML = 'You won';
         userScore.innerHTML++;
         leftRound.innerHTML--;
-    } else if ((playerChoice === 'rock' && computerChoice === 'paper') || (playerChoice === 'scissors' && computerChoice === 'rock') || (playerChoice === 'paper' && computerChoice === 'scissors')) {
+    } else if ((computerChoice === 'paper') && (playerChoice === 'rock') || ((computerChoice === 'rock') && (playerChoice === 'scissors')) || ((computerChoice === 'scissors') && (playerChoice === 'paper'))) {
         resultMsg.innerHTML = 'You lost';
         computerScore.innerHTML++;
         leftRound.innerHTML--;
@@ -73,7 +88,7 @@ var parallel = function(playerChoice, computerChoice) {
 
 
 var reset = function() {
-    leftRound.innerHTML = 'number of rounds!'
+    //    leftRound.innerHTML = 'number of rounds!'
     resultMsg.innerHTML = 'start game';
     userScore.innerHTML = 0;
     computerScore.innerHTML = 0;
@@ -87,5 +102,15 @@ var reset = function() {
 newGame.addEventListener('click', function() {
     reset();
     var numbRound = window.prompt('how many times do you want to play?  -  type number');
-    return leftRound.innerHTML = numbRound;
+    if (isNaN(numbRound) || (numbRound === '') || (numbRound === null)) {
+        resultMsg.innerHTML = 'Just type number remember';
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+    } else {
+        return leftRound.innerHTML = numbRound;
+        rock.disabled = false;
+        paper.disabled = false;
+        scissors.disabled = false;
+    }
 });
